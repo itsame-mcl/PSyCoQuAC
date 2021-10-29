4c48ab298f54044dd58994f8c3e0d19fd582e6df
Lucas Macaux <lucas.macaux@orange.fr>
2021-10-29 11:49:43 +0200

    Finition de session.py et de session_service.py

--- BusinessLayer/BusinessObjects/session.py
+++ BusinessLayer/BusinessObjects/session.py
@@ -1,13 +1,13 @@
 class Session:
 
-    def __init__(self, utilisateur_connecte, droits_utilisateurs):
+    def __init__(self, utilisateur_connecte, droits_superviseurs):
         self._utilisateur_connecte = utilisateur_connecte
-        self._droits_utilisateurs = droits_utilisateurs
+        self._droits_superviseurs = droits_superviseurs
 
     @property
     def utilisateur_connecte(self):
         return self._utilisateur_connecte # ce getter renvoie un Agent
     
     @property
-    def droits_utilisateurs(self):
-        return self._droits_utilisateurs
+    def droits_superviseurs(self):
+        return self._droits_superviseurs
--- BusinessLayer/LocalServices/Gestion/session_service.py
+++ BusinessLayer/LocalServices/Gestion/session_service.py
@@ -1,4 +1,5 @@
 from session.py import Session
+import DAO as dao
 from hashlib import sha512
 
 @Singleton
@@ -7,14 +8,21 @@ class SessionServices:
     def ouvrir_session(nom_utilisateur, mot_de_passe):
         hache = sha512(nom_utilisateur + mot_de_passe)
         # Avec la DAO, on récupère le mot de passe sauvegardé dans la BDD qui correspond à l'id de l'agent
-        mdp = 
-        if hache == mdp:
-            agent_id = recuperer_agent_id(nom_utilisateur, mot_de_passe) # On fait appel à une méthode de la DAO qui récupère l'id de l'agent
-            identité = recuperer_agent_identite(nom_utilisateur, mot_de_passe) # On fait appel à une méthode de la DAO qui récupère le prénom et le nom de l'agent
+        mdp = dao.recuperer_agent_mdp(nom_utilisateur, mot_de_passe) # On peut se demander s'il ne vaudrait pas mieux renseigner un id, 
+                                                    # pour faire la recherche du mdp haché dans la BDD
+        if hache == mdp: # si le mdp récupéré dans la BDD est le même que le salage_hacahage, l'agent peut ouvrir sa session
+            # Pour ouvrir une session, on crée un Agent, et on lui donne un booléen (True si l'Agent est un superviseur, False sinon)
+            # Pour créer un Agent, on a besoin : de son id, de son nom_utilisateur, de (nom, prénom) et de l'id de son superviseur
+            agent_id = dao.recuperer_agent_id(hache) # On fait appel à une méthode de la DAO qui récupère l'id de l'agent
+            identite = dao.recuperer_agent_identite(hache) # On fait appel à une méthode de la DAO qui récupère le prénom et le nom de l'agent
             # On fait appel à une méthode de la DAO qui regarde si l'Agent qui se connecte est un gestionnaire ou un superviseur
-            if regarder_si_superviseur(nom_utilisateur, mot_de_passe):
-                return Session()
+            if dao.regarder_si_superviseur(hache): # la méthode regarder_si_superviseur renvoie True si l'Agent est un superviseur
+                from Superviseur.py import Superviseur
+                equipe_deleguee = dao.recuperer_equipe_deleguee(hache)
+                return Session(Superviseur(agent_id, nom_utilisateur, identite, equipe_deleguee), True)
             else: # l'Agent est un gestionnaire
-                return Session()
+                from Gestionnaire.py import Gestionnaire
+                id_superviseur = dao.recuperer_id_superviseur(hache)
+                return Session(Gestionnaire(agent_id, nom_utilisateur, identite, id_superviseur), False)
         else:
-            return "Vous n'avez pas dis le mot magique !"
+            return "Ah ah ah ! Vous n'avez pas dis le mot magique !"
