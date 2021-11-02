091875ab020d4d5e87097893452d2741fabd5a5d
Lucas Macaux <lucas.macaux@orange.fr>
2021-11-02 10:37:31 +0100

    Correction d'une faute de frappe dans Gestionnaire & Superviseur

--- BusinessLayer/BusinessObjects/Superviseur.py
+++ BusinessLayer/BusinessObjects/Superviseur.py
@@ -2,7 +2,7 @@ from BusinessLayer.BusinessObjects.Agent import Agent
 
 class Gestionnaire(Agent):
 
-    def __init__(self, agent_id, nom_utilisateur, identite, equipe_deleguee_a)
+    def __init__(self, agent_id, nom_utilisateur, identite, equipe_deleguee_a):
         super().__init__(agent_id, nom_utilisateur, identite)
         self._equipe_deleguee_a = equipe_deleguee_a
 
