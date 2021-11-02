252801acb8460d8db67b081b3759e3cc0874f152
Touzé <id1755@ensai.fr>
2021-11-02 16:19:27 +0100

    modification nom du paramètre nb_fiche_a_echantilloner

--- BusinessLayer/LocalServices/TraitementFA/AffectationService.py
+++ BusinessLayer/LocalServices/TraitementFA/AffectationService.py
@@ -1,12 +1,12 @@
 from random import *
 
 class AffectationService():
-    def echantilloner_fiches(echantillon, taille_echantillon):
+    def echantilloner_fiches(echantillon, nb_fiche_à_controler):
         n = len(echantillon)
         l = [i for i in range (n)]
-        liste_aleatoire = sample(l, taille_echantillon) #Contruction d'une liste aléatoire d'entier compris entre 0 et n-1 de taille taille_echantillon 
+        liste_aleatoire = sample(l, nb_fiche_à_controler) #Contruction d'une liste aléatoire d'entier compris entre 0 et n-1 de taille nb_fiche_à_controler 
         liste_echantillonne = []
-        for i in range (taille_echantillon) :
+        for i in range (nb_fiche_à_controler) :
             liste_echantillonne.append(echantillon[liste_aleatoire[i]])
         return liste_echantillonne
 
