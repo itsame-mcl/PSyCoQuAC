7398aa0f34c1ebf3e7da08e41fc744d7708b2f5f
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-11-02 12:52:54 +0100

    Suite DAO FicheAdresse

--- DataLayer/DAO/dao_fiche_adresse.py
+++ DataLayer/DAO/dao_fiche_adresse.py
@@ -1,4 +1,5 @@
 import dotenv
+from datetime import date
 
 from BusinessLayer.BusinessObjects.adresse import Adresse
 from BusinessLayer.BusinessObjects.fiche_adresse import FicheAdresse
@@ -12,7 +13,7 @@ class DAOFicheAdresse(metaclass=Singleton):
         if engine == "SQLite":
             self.__interface = SQLiteFicheAdresse()
 
-    def recuperer_fiche_adresse(self, identifiant):
+    def recuperer_fiche_adresse(self, identifiant : int) -> FicheAdresse:
         data = self.__interface.recuperer_fiche_adresse(identifiant)
         adresse_initiale = Adresse(data["initial_numero"], data["initial_voie"], data["initial_code_postal"],
                                    data["initial_ville"])
@@ -23,3 +24,22 @@ class DAOFicheAdresse(metaclass=Singleton):
                           data["code_resultat"])
         return fa
 
+    def creer_fiche_adresse(self, fa : FicheAdresse) -> bool:
+        data = dict()
+        data["identifiant_pot"] = fa.agent_id
+        data["identifiant_lot"] = fa.lot_id
+        data["code_resultat"] = fa.code_res
+        data["date_importation"] = date.today()
+        data["date_dernier_traitement"] = date.today()
+        data["initial_numero"] = fa.adresse_initiale.numero
+        data["initial_voie"] = fa.adresse_initiale.voie
+        data["initial_code_postal"] = fa.adresse_initiale.cp
+        data["initial_ville"] = fa.adresse_initiale.ville
+        data["final_numero"] = fa.adresse_finale.numero
+        data["final_voie"] = fa.adresse_finale.voie
+        data["final_code_postal"] = fa.adresse_finale.cp
+        data["final_ville"] = fa.adresse_finale.ville
+        data["coordonnees_wgs84"] = fa.coords_wgs84
+        data["champs_supplementaires"] = fa.champs_supplementaires
+        res = self.__interface.creer_fiche_adresse(data)
+        return res
--- DataLayer/DAO/interface_fiche_adresse.py
+++ DataLayer/DAO/interface_fiche_adresse.py
@@ -3,5 +3,8 @@ from abc import ABC, abstractmethod
 
 class InterfaceFicheAdresse(ABC):
     @abstractmethod
-    def recuperer_fiche_adresse(self, identifiant):
+    def recuperer_fiche_adresse(self, identifiant : int) -> dict:
+        raise NotImplementedError
+
+    def creer_fiche_adresse(self, dictionnaire : dict) -> bool:
         raise NotImplementedError
