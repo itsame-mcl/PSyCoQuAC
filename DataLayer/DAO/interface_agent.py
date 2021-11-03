298e6a9e777b1d80a738853c631700d3abc3fae5
Lucas Macaux <lucas.macaux@orange.fr>
2021-11-03 15:00:40 +0100

    NotImplementedError

--- DataLayer/DAO/interface_agent.py
+++ DataLayer/DAO/interface_agent.py
@@ -6,32 +6,28 @@ class InterfaceAgent(ABC):
 
     @abstractmethod
     def deleguer_agent_a(self, id_agents : List[int], id_superviseur_actuel : int, id_superviseur_futur : int):
-        raise NotImplemented
+        raise NotImplementedError
 
     @abstractmethod
     def recuperer_liste_agents(self, id_superviseur : int) -> List[Agent]:
-        raise NotImplemented
+        raise NotImplementedError
 
     @abstractmethod
     def creer_agent(self, session_utilisateur : Session, prenom : varchar(50), nom : varchar(100), nom_utilisateur : varchar(20), mot_de_passe : char(128), est_superviseur : bool) ->:
-        raise NotImplemented
+        raise NotImplementedError
 
     @abstractmethod
     def modifier_agent(self, agent_a_modifier : Agent) -> bool:
-        raise NotImplemented
+        raise NotImplementedError
 
     @abstractmethod
     def changer_droits(self, agent_a_modifier : Agent) -> bool:
-        raise NotImplemented
+        raise NotImplementedError
 
     @abstractmethod
     def supprimer_agent(self, id_agent : int) -> bool:
-        raise NotImplemented
-
-    @abstractmethod
-    def est_superviseur(self, nom_utilisateur : varchar(20)) -> bool:
-        raise NotImplemented
+        raise NotImplementedError
 
     @abstractmethod
     def recuperer_mdp_agent(self, nom_utilisateur : varchar(20), mot_de_passe : varchar(100)) -> char(128):
-        raise NotImplemented
+        raise NotImplementedError
