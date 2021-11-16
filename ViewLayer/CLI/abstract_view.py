1bfb56dead1f538ec3b48559fdd1a938a496e79d
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-11-16 21:41:29 +0100

    Modif vue abstraite

--- ViewLayer/CLI/abstract_view.py
+++ ViewLayer/CLI/abstract_view.py
@@ -2,10 +2,9 @@ from abc import ABC, abstractmethod
 
 class AbstractView(ABC):
 
-    @abstractmethod
     def display_info(self):
         pass
 
     @abstractmethod
     def make_choice(self):
-        pass
+        raise NotImplementedError
