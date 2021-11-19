a089179a4929523cb60c0e2a958261cbdc849617
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-11-19 09:42:53 +0100

    importation_service est un singleton

--- BusinessLayer/LocalServices/IO/importation_service.py
+++ BusinessLayer/LocalServices/IO/importation_service.py
@@ -4,12 +4,10 @@ import BusinessLayer.LocalServices.IO.factory_handler as factory
 import pathlib
 
 
-@Singleton
-class ImportationService:
+class ImportationService(metaclass=Singleton):
 
     def importer_lot(self, id_agent, chemin_fichier):
         path = pathlib.Path(chemin_fichier)
         handler = factory.HandlerFactory.get_handler_from_ext(path.suffixes[-1])
         id_lot = DAOFicheAdresse().recuperer_dernier_id_lot() + 1
         liste_fa = handler.import_from_file(chemin_fichier, id_agent, id_lot)
-
