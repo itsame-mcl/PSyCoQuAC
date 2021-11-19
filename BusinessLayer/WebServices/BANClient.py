32ebdede3cf0eaa3288a915635e9a9db3e416a37
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-11-19 11:02:13 +0100

    Gestion des temps d'attente sur l'API

--- BusinessLayer/WebServices/BANClient.py
+++ BusinessLayer/WebServices/BANClient.py
@@ -1,16 +1,23 @@
 from typing import List, Tuple
 import requests
 
+from time import time_ns, sleep
 from BusinessLayer.BusinessObjects.fiche_adresse import FicheAdresse
 from utils.singleton import Singleton
 
 
 class BANClient(metaclass=Singleton):
-    @staticmethod
-    def geocodage_par_fiche(fiche_a_traiter: FicheAdresse) -> Tuple[float, FicheAdresse]:
+    def __init__(self):
+        self.__wait_ns = 2000000
+        self.__lastcall = 0
+
+    def geocodage_par_fiche(self, fiche_a_traiter: FicheAdresse) -> Tuple[float, FicheAdresse]:
         adresse = fiche_a_traiter.adresse_initiale
         adresse = adresse.replace(" ", "+")
+        if time_ns() - self.__lastcall < self.__wait_ns:
+            sleep((time_ns() - self.__lastcall)/1000000000)
         response = requests.get("https://api-adresse.data.gouv.fr/search/?q=" + str(adresse))
+        self.__lastcall = time_ns()
         response = response.json()
         score = response["score"]
         coordonnees_gps = response["coordinates"]
@@ -21,14 +28,16 @@ class BANClient(metaclass=Singleton):
         fiche_a_traiter.adresse_finale = adresse_api
         return score, fiche_a_traiter
 
-    @staticmethod
-    def reverse_par_fiche(fiche_a_traiter: FicheAdresse) -> Tuple[float, FicheAdresse]:
+    def reverse_par_fiche(self, fiche_a_traiter: FicheAdresse) -> Tuple[float, FicheAdresse]:
         if len(fiche_a_traiter.coords_wgs84) == 0:
             raise ValueError('La FicheAdresse ne possède pas de coordonnées GPS')
         coordonnees_gps = fiche_a_traiter.coords_wgs84
         lon = coordonnees_gps[0]
         lat = coordonnees_gps[1]
+        if time_ns() - self.__lastcall < self.__wait_ns:
+            sleep((time_ns() - self.__lastcall)/1000000000)
         response = requests.get("https://api-adresse.data.gouv.fr/reverse/?lon=" + str(lon) + "&lat=" + str(lat))
+        self.__lastcall = time_ns()
         response = response.json()
         score = response["score"]
         numero_rue = response["name"]
@@ -37,22 +46,20 @@ class BANClient(metaclass=Singleton):
         fiche_a_traiter.adresse_finale = adresse_api
         return score, fiche_a_traiter
 
-    @staticmethod
-    def geocodage_par_lot(fiches_a_traiter: List[FicheAdresse]) -> Tuple[List[float], List[FicheAdresse]]:
+    def geocodage_par_lot(self, fiches_a_traiter: List[FicheAdresse]) -> Tuple[List[float], List[FicheAdresse]]:
         n = len(fiches_a_traiter)
         scores = []
         for i in range(n):
-            score, fiche = BANClient.geocodage_par_fiche(fiches_a_traiter[i])
+            score, fiche = self.geocodage_par_fiche(fiches_a_traiter[i])
             fiches_a_traiter[i] = fiche
             scores.append(score)
         return scores, fiches_a_traiter
 
-    @staticmethod
-    def reverse_par_lot(fiches_a_traiter: List[FicheAdresse]) -> Tuple[List[float], List[FicheAdresse]]:
+    def reverse_par_lot(self, fiches_a_traiter: List[FicheAdresse]) -> Tuple[List[float], List[FicheAdresse]]:
         n = len(fiches_a_traiter)
         scores = []
         for i in range(n):
-            score, fiche = BANClient.reverse_par_fiche(fiches_a_traiter[i])
+            score, fiche = self.reverse_par_fiche(fiches_a_traiter[i])
             fiches_a_traiter[i] = fiche
             scores.append(score)
         return scores, fiches_a_traiter
