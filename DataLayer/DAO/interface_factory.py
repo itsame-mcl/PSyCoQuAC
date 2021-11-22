0a5ba2d9cd79d64cf17fba676076f89e176b222f
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-11-22 21:27:28 +0100

    MaJ Interface Factory DAO

--- DataLayer/DAO/interface_factory.py
+++ DataLayer/DAO/interface_factory.py
@@ -4,10 +4,13 @@ import DataLayer.DAO.sqlite_fiche_adresse as slfa
 import DataLayer.DAO.sqlite_modele as slmo
 import DataLayer.DAO.sqlite_agent as slag
 import DataLayer.DAO.pg_connexion as pgcx
+import DataLayer.DAO.pg_fiche_adresse as pgfa
+import DataLayer.DAO.pg_modele as pgmo
+import DataLayer.DAO.pg_agent as pgag
+from utils.singleton import Singleton
 
 
-class InterfaceFactory:
-
+class InterfaceFactory(metaclass=Singleton):
     @staticmethod
     def get_interface(type_dao: str):
         if os.environ["PSYCOQUAC_ENGINE"] == "SQLite":
@@ -24,6 +27,12 @@ class InterfaceFactory:
         elif os.environ["PSYCOQUAC_ENGINE"] == "PostgreSQL":
             if type_dao == "Connexion":
                 return pgcx.PGConnexion()
+            elif type_dao == "FicheAdresse":
+                return pgfa.PGFicheAdresse()
+            elif type_dao == "Modele":
+                return pgmo.PGModele()
+            elif type_dao == "Agent":
+                return pgag.PGAgent()
             else:
                 raise NotImplementedError
         else:
