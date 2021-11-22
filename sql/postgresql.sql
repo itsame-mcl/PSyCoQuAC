c44f66ee4743b5d57fd8ae4e2f58ff6ceeb05e7f
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-11-22 18:04:08 +0100

    Script de base pour PG + màj script sqlite

---
+++ sql/postgresql.sql
@@ -0,0 +1,51 @@
+DROP TABLE IF EXISTS fa;
+CREATE TABLE IF NOT EXISTS fa (
+    identifiant_fa SERIAL PRIMARY KEY,
+    identifiant_pot INTEGER NOT NULL,
+    identifiant_lot INTEGER NOT NULL,
+    code_resultat CHAR(2) NOT NULL,
+    date_importation DATE NOT NULL,
+    date_dernier_traitement DATE NOT NULL,
+    initial_numero VARCHAR(10),
+    initial_voie VARCHAR(100),
+    initial_code_postal VARCHAR(10),
+    initial_ville VARCHAR(100),
+    final_numero VARCHAR(10),
+    final_voie VARCHAR(100),
+    final_code_postal VARCHAR(10),
+    final_ville VARCHAR(100),
+    coordonnees_wgs84 POINT,
+    champs_supplementaires TEXT[][]
+);
+
+DROP SEQUENCE IF EXISTS identifiant_lot_seq;
+CREATE SEQUENCE identifiant_lot_seq;
+
+DROP TABLE IF EXISTS agents;
+CREATE TABLE IF NOT EXISTS agents (
+    identifiant_agent SERIAL PRIMARY KEY,
+    est_superviseur BOOLEAN NOT NULL,
+    quotite DECIMAL NOT NULL,
+    identifiant_superviseur INTEGER,
+    identifiant_delegue INTEGER,
+    nom_utilisateur VARCHAR(64) UNIQUE NOT NULL,
+    mot_de_passe CHAR(128) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    nom VARCHAR(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS modeles;
+CREATE TABLE IF NOT EXISTS modeles (
+    identifiant_modele SERIAL PRIMARY KEY,
+    nom_modele VARCHAR(50) NOT NULL,
+    regex_nom_fichier VARCHAR(255) NOT NULL,
+    position_champ_numero SMALLINT NOT NULL,
+    position_champ_voie SMALLINT NOT NULL,
+    position_champ_code_postal SMALLINT NOT NULL,
+    position_champ_ville SMALLINT NOT NULL,
+    position_champs_supplementaires TEXT[][]
+);
+
+INSERT INTO modeles (nom_modele, regex_nom_fichier, position_champ_numero,
+                     position_champ_voie, position_champ_code_postal, position_champ_ville)
+VALUES ('Modèle par défaut','.*',0,1,2,3);
--- sql/sqlite.sql
+++ sql/sqlite.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS fa;
 CREATE TABLE IF NOT EXISTS fa (
     identifiant_fa INTEGER PRIMARY KEY AUTOINCREMENT,
-    identifiant_pot INTEGER,
-    identifiant_lot INTEGER,
-    code_resultat TEXT,
-    date_importation TEXT,
-    date_dernier_traitement TEXT,
+    identifiant_pot INTEGER NOT NULL,
+    identifiant_lot INTEGER NOT NULL,
+    code_resultat TEXT NOT NULL,
+    date_importation TEXT NOT NULL,
+    date_dernier_traitement TEXT NOT NULL,
     initial_numero TEXT,
     initial_voie TEXT,
     initial_code_postal TEXT,
@@ -24,6 +24,7 @@ CREATE TABLE IF NOT EXISTS agents (
     est_superviseur INTEGER NOT NULL,
     quotite NUMERIC NOT NULL,
     identifiant_superviseur INTEGER,
+    identifiant_delegue INTEGER,
     nom_utilisateur TEXT UNIQUE NOT NULL,
     mot_de_passe TEXT NOT NULL,
     prenom TEXT NOT NULL,
@@ -34,22 +35,22 @@ DROP TABLE IF EXISTS modeles;
 CREATE TABLE IF NOT EXISTS modeles (
     identifiant_modele INTEGER PRIMARY KEY AUTOINCREMENT,
     nom_modele TEXT NOT NULL,
-    regex_nom_fichier TEXT,
-    position_champ_numero TEXT,
-    position_champ_voie TEXT,
-    position_champ_code_postal TEXT,
-    position_champ_ville TEXT,
+    regex_nom_fichier TEXT NOT NULL,
+    position_champ_numero TEXT NOT NULL,
+    position_champ_voie TEXT NOT NULL,
+    position_champ_code_postal TEXT NOT NULL,
+    position_champ_ville TEXT NOT NULL,
     position_champs_supplementaires TEXT
 );
 
 INSERT INTO sqlite_sequence(name, seq)
 VALUES
-    ("fa",0),
-    ("agents",0),
-    ("modeles",0),
-    ("lots",0);
+    ('fa',0),
+    ('agents',0),
+    ('modeles',0),
+    ('lots',0);
 
 INSERT INTO modeles(nom_modele, regex_nom_fichier, position_champ_numero,
                     position_champ_voie, position_champ_code_postal,
                     position_champ_ville,position_champs_supplementaires)
-VALUES ("defaut",".*","(0)","(1)","(2)","(3)","{}");
+VALUES ('Modèle par défaut','.*','(0)','(1)','(2)','(3)','{}');
