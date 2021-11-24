b6fd1d0c8213e913dd54c8120fe64e0bd8a3099b
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-11-24 21:40:05 +0100

    PEP

--- ViewLayer/CLI/connexion_view.py
+++ ViewLayer/CLI/connexion_view.py
@@ -12,13 +12,15 @@ class ConnexionView(AbstractView):
         self.__questions = [{'type': 'input', 'name': 'nom_utilisateur', 'message': "Nom d'utilisateur"},
                             {'type': 'password', 'name': 'mot_de_passe', 'message': 'Mot de passe :'}]
         self.__prompt_reessayer = [{'type': 'confirm', 'name': 'retry', 'message': "Nom d'utilisateur ou "
-                "mot de passe incorrect. Voulez-vcus réessayer ?", 'default': True}]
+                                                                                   "mot de passe incorrect."
+                                                                                   "Voulez-vcus réessayer ?",
+                                    'default': True}]
 
     def make_choice(self):
         answers = prompt(self.__questions)
-        agent = SessionService().ouvrir_session(answers['nom_utilisateur'],answers['mot_de_passe'])
+        agent = SessionService().ouvrir_session(answers['nom_utilisateur'], answers['mot_de_passe'])
         if not (isinstance(agent, Agent)):
-            #Ah ah ah... Vous n'avez pas dis le mot magique !
+            # Ah ah ah... Vous n'avez pas dis le mot magique !
             reessayer = prompt(self.__prompt_reessayer)
             if reessayer['retry']:
                 return ConnexionView()
