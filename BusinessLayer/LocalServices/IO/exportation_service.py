8e017f3a75f65a0db1dff6549d9f5935c328f790
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-11-26 18:27:03 +0100

    Hotfix exportation_service.py

--- BusinessLayer/LocalServices/IO/exportation_service.py
+++ BusinessLayer/LocalServices/IO/exportation_service.py
@@ -20,7 +20,6 @@ class ExportationService(metaclass=Singleton):
         path = pathlib.Path(chemin_fichier).resolve()
         try:
             handler = factory.HandlerFactory.get_handler_from_ext(path.suffix)
-            path.touch(exist_ok=True)
             lot = DAOFicheAdresse().recuperer_lot(id_lot)
             handler.export_to_file(lot, chemin_fichier)
         except AttributeError:
